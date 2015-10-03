fa867c198a9275b62bfe8e4ca3186a8fcd244982
Ubuntu <ubuntu@ip-172-31-4-26.us-west-1.compute.internal>
2015-10-03 22:55:05 +0000

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app --log-file=-
+web: python index01.py
--- index01.py
+++ index01.py
@@ -7,4 +7,4 @@ def hello_world():
     return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=5000)
