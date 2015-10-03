4887f74fe690b4ca20207a32805eac7504189c40
Ubuntu <ubuntu@ip-172-31-4-26.us-west-1.compute.internal>
2015-10-03 23:00:31 +0000

    test1

--- index01.py
+++ index01.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from ocr import process_image
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def hello_world():
     return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=5000)
+	port=int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0',port=port)
