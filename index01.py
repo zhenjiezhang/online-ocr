b042ef98564b0ad3d528f2bcd0d5388ace41e9d3
Ubuntu <ubuntu@ip-172-31-4-26.us-west-1.compute.internal>
2015-10-03 23:48:29 +0000

    fdf

--- index01.py
+++ index01.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask
-# from ocr import process_image
+from ocr import process_image
 app = Flask(__name__)
 
 
-    # return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
 
 @app.route('/')
 def hello():
-    return "Hello from Python!"
+    return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
+	
+    # return "Hello from Python!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
