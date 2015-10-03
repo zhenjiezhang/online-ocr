ccaf1e2e4d513dbe0f6173fcf7d741cf08e70e5b
Ubuntu <ubuntu@ip-172-31-4-26.us-west-1.compute.internal>
2015-10-03 23:06:32 +0000

    test2

--- index01.py
+++ index01.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
+    # return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
+    return "hello ginger"
 
 if __name__ == '__main__':
 	port=int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0',port=port)
