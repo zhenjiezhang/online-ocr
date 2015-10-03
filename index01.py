d9c97241635f4153df399f3a9b4a0be3ad1405db
Ubuntu <ubuntu@ip-172-31-4-26.us-west-1.compute.internal>
2015-10-03 23:43:56 +0000

    fdf

--- index01.py
+++ index01.py
@@ -3,12 +3,13 @@ from flask import Flask
 # from ocr import process_image
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    # return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
-    return "hello ginger"
 
-if __name__ == '__main__':
-	port=int(os.environ.get("PORT", 5000))
-	
-    app.run(host='0.0.0.0',port=port)
+    # return process_image('http://www.nhc.noaa.gov/gifs/WindSpeedProbText2_sm.gif')
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
